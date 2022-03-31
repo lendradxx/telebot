a83d4219402b6c2053119b06e9c41e73b97c01c1
Lendra <lendev0406@gmail.com>
2022-03-31 17:04:49 +0700

    adding basic command

--- src/components/handler.py
+++ src/components/handler.py
@@ -1,4 +1,12 @@
 from telebot import TeleBot, types
 
+
 def textHandler(bot: TeleBot, msg: types.Message):
-    pass
+    command = msg.text[1:len(msg.text)].strip().split(" ").pop(0).lower()
+
+    if command == "hello":
+        bot.reply_to(msg, "Hello!")
+    elif command == "yo":
+        bot.reply_to(msg, "Yo!")
+    else:
+        pass
--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-import os
 from telebot import TeleBot, types
 from utils import core
 from components import handler
@@ -6,14 +5,16 @@ from components import handler
 if __name__ == "__main__":
     bot = TeleBot(core.getConfig("BOT", "TOKEN"))
     # Print Info Bot
+    PREFIX = core.getConfig("BOT", "PREFIX")
     core.printHeaderLine("=")
     print(f"BOT NAME: @{bot.get_me().username}")
-    print(f"PREFIX: {core.getConfig('BOT', 'PREFIX')}")
+    print(f"PREFIX: {PREFIX}")
     core.printHeaderLine("=")
 
     # Handling message
     @bot.message_handler(content_types="text")
     def textHandler(message: types.Message):
+        if not message.text.startswith(PREFIX): return
         if core.lateMsg(message.date): return
         handler.textHandler(bot=bot, msg=message)
 
