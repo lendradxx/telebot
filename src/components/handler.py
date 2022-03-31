d4cd1a53a0dc51e6fda374d0b776807f2f8bf0f1
Lendra <lendev0406@gmail.com>
2022-03-31 17:13:23 +0700

    add say command and args

--- src/components/handler.py
+++ src/components/handler.py
@@ -3,10 +3,13 @@ from telebot import TeleBot, types
 
 def textHandler(bot: TeleBot, msg: types.Message):
     command = msg.text[1:len(msg.text)].strip().split(" ").pop(0).lower()
+    args = msg.text[1:len(msg.text)].strip().split(" ")[1:len(msg.text)]
 
     if command == "hello":
         bot.reply_to(msg, "Hello!")
     elif command == "yo":
         bot.reply_to(msg, "Yo!")
+    elif command == "say":
+        bot.send_message(msg.chat.id, " ".join(args))
     else:
         pass
