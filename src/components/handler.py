7dc4133dc14673b53b627c2326e802885e71a613
Lendra <lendev0406@gmail.com>
2022-04-01 17:46:57 +0700

    Update handler.py

--- src/components/handler.py
+++ src/components/handler.py
@@ -1,6 +1,5 @@
 from telebot import TeleBot, types
 
-
 def textHandler(bot: TeleBot, msg: types.Message):
     command = msg.text[1:len(msg.text)].strip().split(" ").pop(0).lower()
     args = msg.text[1:len(msg.text)].strip().split(" ")[1:len(msg.text)]
@@ -9,5 +8,7 @@ def textHandler(bot: TeleBot, msg: types.Message):
         bot.reply_to(msg, "Hello!")
     elif command == "say":
         bot.send_message(msg.chat.id, " ".join(args))
+    elif command == "start":
+        pass
     else:
         pass
