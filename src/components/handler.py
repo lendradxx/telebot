eb246cf394b6d2f60d882500c921512d9eaab5d3
Lendra <lendev0406@gmail.com>
2022-04-01 19:32:36 +0700

    Update handler.py

--- src/components/handler.py
+++ src/components/handler.py
@@ -1,5 +1,6 @@
 from telebot import TeleBot, types
 
+
 def textHandler(bot: TeleBot, msg: types.Message):
     command = msg.text[1:len(msg.text)].strip().split(" ").pop(0).lower()
     args = msg.text[1:len(msg.text)].strip().split(" ")[1:len(msg.text)]
